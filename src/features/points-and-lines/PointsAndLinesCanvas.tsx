d6ad22f6bb4e40281f3128445f97604c4c0c3a32
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-11-24 23:58:48 +0100

    Draw curve - WIP

--- src/features/points-and-lines/PointsAndLinesCanvas.tsx
+++ src/features/points-and-lines/PointsAndLinesCanvas.tsx
@@ -25,16 +25,21 @@ export function PointsAndLinesCanvas() {
 
     const [location, setLocation] = useState({ x: 0, y: 0 });
     const [points, setPoints] = useState<Shape[]>([]);
+    const [selectedPoints, setSelectedPoints] = useState<Shape[]>([]);
     const [connectingLines, setConnectingLines] = useState<Line[]>([]);
     const [line, setLine] = useState({
         startPos: { x: 0, y: 0 },
+        endPos: { x: 0, y: 0 },
+    });
+    const [curvedLine, setCurvedLine] = useState({
+        startPos: { x: 0, y: 0 },
+        controlPos: { x: 0, y: 0 },
         endPos: { x: 0, y: 0 },
     });
     const [isDrawingLine, setIsDrawingLine] = useState(false);
     const [pointSize, setPointSize] = useState(8);
 
     const handleCanvasClick = (e: FederatedPointerEvent) => {
-        console.log('canvas');
         if (isDrawingLine) {
             setIsDrawingLine(false);
 
@@ -50,6 +55,7 @@ export function PointsAndLinesCanvas() {
 
     function handleClearBtnClick() {
         setPoints([]);
+        setSelectedPoints([]);
         setConnectingLines([])
     }
 
@@ -59,40 +65,46 @@ export function PointsAndLinesCanvas() {
         console.log(line.startPos);
     }
 
-    function handlePointClick(e: FederatedPointerEvent) {
-        console.log('point');
+    function handlePointClick(selectedPoint: Point) {
+        // const xPos = e.target.x + e.target.width / 2;
+        // const yPos = e.target.y + e.target.height / 2;
 
-        const xPos = e.target.x + e.target.width / 2;
-        const yPos = e.target.y + e.target.height / 2;
+        // if (isDrawingLine) {
+        //     setConnectingLines(prev => [
+        //         ...prev,
+        //         {
+        //             startPos: { x: line?.startPos.x, y: line?.startPos.y },
+        //             endPos: { x: xPos, y: yPos }
+        //         }
+        //     ]);
+        //     setIsDrawingLine(false);
 
-        if (isDrawingLine) {
-            setConnectingLines(prev => [
-                ...prev,
-                {
-                    startPos: { x: line?.startPos.x, y: line?.startPos.y },
-                    endPos: { x: xPos, y: yPos }
-                }
-            ]);
-            setIsDrawingLine(false);
+        //     return;
+        // }
 
-            return;
-        }
-
-        setLine({
-            startPos: { x: xPos, y: yPos },
-            endPos: { x: xPos, y: yPos }
-        });
-        setIsDrawingLine(true);
+        // setLine({
+        //     startPos: { x: xPos, y: yPos },
+        //     endPos: { x: xPos, y: yPos }
+        // });
+        // setIsDrawingLine(true);
     }
 
     function handleMouseMove(e: FederatedPointerEvent) {
         if (!isDrawingLine)
             return;
 
-        setLine(prev => ({
-            ...prev,
-            endPos: { x: e.global.x, y: e.global.y }
-        }));
+        // setLine(prev => ({
+        //     ...prev,
+        //     endPos: { x: e.global.x, y: e.global.y }
+        // }));
+    }
+
+    function handleDrawCurve() {
+        setCurvedLine({
+            startPos: { x: selectedPoints[0].x, y:  selectedPoints[0].y },
+            controlPos: { x:  selectedPoints[1].x, y:  selectedPoints[1].y },
+            endPos: { x:  selectedPoints[2].x, y:  selectedPoints[2].y },
+        });
     }
 
     return (
@@ -104,8 +116,16 @@ export function PointsAndLinesCanvas() {
                     value={pointSize}
                     onChange={(e) => setPointSize(parseInt(e.target.value))}
                     className="border border-zinc-400 rounded-xs px-2 py-1" />
-                <button className="bg-blue-500 px-2 py-1 rounded-xs hover:bg-blue-400" onClick={handleClearBtnClick}>Clear</button>
-                <button className="bg-blue-500 px-2 py-1 rounded-xs hover:bg-blue-400" onClick={handlePrintBtnClick}>Print</button>
+                <button
+                    className="bg-blue-500 px-2 py-1 rounded-xs hover:bg-blue-400"
+                    onClick={handleClearBtnClick}>Clear</button>
+                <button
+                    className="bg-blue-500 px-2 py-1 rounded-xs hover:bg-blue-400"
+                    onClick={handlePrintBtnClick}>Print</button>
+                <button
+                    className="bg-blue-500 px-2 py-1 rounded-xs hover:bg-blue-400"
+                    onClick={handleDrawCurve}
+                    disabled={selectedPoints.length < 3}>Draw curve</button>
             </div>
             <Application
                 width={canvasWidth}
@@ -133,7 +153,7 @@ export function PointsAndLinesCanvas() {
                         size={s.size}
                         color='red'
                         eventMode='static'
-                        onClick={handlePointClick} />
+                        onClick={() => handlePointClick(s)} />
                 ))}
                 {connectingLines.map(l => (
                     <pixiGraphics
